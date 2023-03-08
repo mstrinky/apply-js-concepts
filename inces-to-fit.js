86cdc6624571b53df824339e4f4873cd5e550f30
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-08 19:44:37 +0600

    inc to feet

---
+++ inces-to-fit.js
@@ -0,0 +1,13 @@
+const myInces = 500;
+const myFeet = myInces/12;
+console.log(myFeet);
+
+
+function inchToFeet(inces){
+    const feet = inces/12;
+    return feet;
+    // console.log(feet);
+}
+
+var dadaFeet = inchToFeet(144);
+console.log(dadaFeet);
