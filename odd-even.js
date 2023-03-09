171cb923006a9f17cafbd89e7d6379b64a26ea89
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-09 14:32:37 +0600

    even and odd number using function

--- odd-even.js
+++ odd-even.js
@@ -1,22 +1,22 @@
-// function isEven(number){
-//     var remainder = number%2;
-//     return remainder;
-// }
-// var numbers = isEven(53);
-// console.log(numbers);
+function isEven(number){
+    var remainder = number%2;
+    return remainder;
+}
+ isEven(53);
 
-// function isEven(number){
-//     var remainder = number%2;
-//     // return remainder;
-//     if(remainder===0){
-//         console.log('the number is even');
-//     }
-//     else{
-//         console.log('the number is odd');
-//     }
-// }
-// var numbers = isEven(57);
-// console.log(numbers);
+
+function isEven(number){
+    var remainder = number%2;
+    // return remainder;
+    if(remainder===0){
+        console.log('the number is even');
+    }
+    else{
+        console.log('the number is odd');
+    }
+}
+ isEven(57);
+
 
 
 function isOdd(number){
