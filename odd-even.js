92cc652442b7d1f175add6b5d916b4a972b17d96
Mst Rinky Akter <mstrinkya@gmail.com>
2023-03-09 14:19:50 +0600

    function

--- odd-even.js
+++ odd-even.js
@@ -1,7 +1,19 @@
+// function isEven(number){
+//     var remainder = number%2;
+//     return remainder;
+// }
+// var numbers = isEven(53);
+// console.log(numbers);
+
 function isEven(number){
     var remainder = number%2;
-    return remainder;
+    // return remainder;
+    if(remainder===0){
+        console.log('the number is even');
+    }
+    else{
+        console.log('the number is odd');
+    }
 }
-var numbers = isEven(53);
+var numbers = isEven(57);
 console.log(numbers);
-
